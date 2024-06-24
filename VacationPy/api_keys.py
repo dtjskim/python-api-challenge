48f81f35c9d20aade2a3bc441f09ba8d055e30ed
dtjskim <david.js.kim@outlook.com>
2024-06-23 17:46:38 -0700

    added folers for each challenge

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "98853773676c805ef72af12ce2dd669b"
+#api_key = "98853773676c805ef72af12ce2dd669b"
+
+# Geoapify API Key
+geoapify_key = "8d7a6c9841f04d8d80dd92d3953c42ab"
